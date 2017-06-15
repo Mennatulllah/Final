691fa07e84e1c91541eb104ad41e8342f787b335
toqaSaeed <toqa.fci@gmail.com>
2017-06-15 13:48:51 +0200

    Home-Didyouknow update

--- src/app/home-didyouknow/home-didyouknow.component.ts
+++ src/app/home-didyouknow/home-didyouknow.component.ts
@@ -9,12 +9,7 @@ export class HomeDidyouknowComponent implements OnInit {
 
   constructor() { }
 ngOnInit (){}
- config: Object = {
-            pagination: '.swiper-pagination',
-            paginationClickable: true,
-            nextButton: '.swiper-button-next',
-            prevButton: '.swiper-button-prev',
-            spaceBetween: 30
-        };
+
+   
 
 }
