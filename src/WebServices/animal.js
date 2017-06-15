536b1066a42404fbdee343dae088459c0da22498
toqaSaeed <toqa.fci@gmail.com>
2017-06-16 00:54:57 +0200

    commit to2a

--- src/WebServices/animal.js
+++ src/WebServices/animal.js
@@ -2,12 +2,6 @@ var router = require('express').Router();
 
 var animalModel = require('../collections/animal');
 
-router.get("/:id", function (request, respone) {
-    animalModel.findById(request.params.id, { __v: 0 })
-        .populate(
-        { path: 'animalType' }
-        ).then(_result => respone.json(_result));
-});
 // any url will start with /animals
 router.get("/", function (request, respone) {
     animalModel.aggregate([
@@ -24,8 +18,16 @@ router.get("/", function (request, respone) {
 });
 
 router.get("/:id", function (request, respone) {
-    animalModel.find({}, { __v: 0 }).then(_result => respone.json(_result));
+    animalModel.findById(request.params.id, { __v: 0 })
+        .populate(
+        { path: 'animalType' }
+        ).then(_result => respone.json(_result));
 });
+
+
+// router.get("/:id", function (request, respone) {
+//     animalModel.find({}, { __v: 0 }).then(_result => respone.json(_result));
+// });
 
 router.post("/", function (request, respone) {
     var _animal = new animalModel(request.body);
